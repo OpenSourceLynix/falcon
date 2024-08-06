a9dd788cc506a44799b95a8977ea016318be8abc
HaTheGamerz <mostwantedhashir+1@gmail.com>
2024-08-06 19:49:40 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** Falcon TailwindCSS Config */
+module.exports = {
+    content: ["./resources/*.ejs", "./resources/**/*.ejs", "./resources/**/**/*.ejs"],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+    ],
+  }
